eabe524e65a6a695d204542f126223162c3cc9d3
Shlomo Auerbach <shlomoa40@gmail.com>
2024-03-14 22:43:26 +0200

    add a func to determen whos turn, add 'isdigit' and not a 'x' or 'o'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from helper import draw_board
+from helper import draw_board , check_turn
 import os
 
 # a dict for every spot on the board
@@ -6,6 +6,7 @@ spots = {1:'1', 2:'2', 3: '3', 4: '4', 5: '5',
           6: '6', 7: '7', 8: '8', 9: '9'}
 
 playing = True
+turn = 0
 
 while playing:
     # clearing the termenal after every turn
@@ -16,4 +17,7 @@ while playing:
     choice = input()
     if choice == 'q':
         playing = False
-
+    elif str.isdigit(choice) and int(choice) in spots:
+        if not spots[int(choice)] in {'X', 'O'}:
+            turn +=1
+            spots[int(choice)] = check_turn(turn)
