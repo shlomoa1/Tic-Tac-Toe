6b2cc31e590f696a5beb5e12153a10507d6ed573
Shlomo Auerbach <shlomoa40@gmail.com>
2024-03-14 22:21:28 +0200

    changing the O and X acording to turn and spot in the board

--- helper.py
+++ helper.py
@@ -2,3 +2,8 @@ def draw_board(spots):
     board = f'|{spots[1]}|{spots[2]}|{spots[3]}|\n|{spots[4]}|{spots[5]}|{spots[6]}|\n|{spots[7]}|{spots[8]}|{spots[9]}|'
     print(board)
 
+def check_turn(turn):
+    if turn % 2 == 0:
+        return 'O'
+    else:
+        return 'X'
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from helper import draw_board
+from helper import draw_board, check_turn
 import os
 
 # a dict for every spot on the board
@@ -6,6 +6,7 @@ spots = {1:'1', 2:'2', 3: '3', 4: '4', 5: '5',
           6: '6', 7: '7', 8: '8', 9: '9'}
 
 playing = True
+turn = 0
 
 while playing:
     # clearing the termenal after every turn
@@ -17,3 +18,5 @@ while playing:
     if choice == 'q':
         playing = False
 
+    turn +=1
+    spots[int(choice)] = check_turn(turn)
